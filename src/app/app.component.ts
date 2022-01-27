49d783ab9df0db3218bef16351ef37871fa34cb0
likhithchowdary26 <likhithchowdary26@gmail.com>
2022-01-27 19:42:15 +0530

    Final Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'counter-app';
+
+  count: number = 10;
+
+  handleIncrease = () =>{
+    if(this.count==10){
+      this.count = 0
+    }
+    else
+      this.count = this.count + 1;
+
+  }
+
+  handleDecrease = () =>{
+    if(this.count==0)
+    this.count =0;
+    else
+    this.count = this.count - 1;
+  }
+
+  handleReset = () =>{
+    this.count = 0;
+  }
+
 }
